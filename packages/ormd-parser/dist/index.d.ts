23c61836c2da3234d44f1ab03e6942109fd0f829
E2 Development Team <dev@nexes.ai>
2025-09-27 17:34:21 -0700

    Remove node_modules and build artifacts from git tracking
    - Remove all node_modules/ directories from git tracking
    - Remove build artifacts (dist/, package-lock.json)
    - Add source files and documentation
    - .gitignore will prevent these from being tracked in future

--- packages/ormd-parser/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @nexes/ormd-parser
- * Parser and validator for ORMD (Organizational Relational Markdown) format
- */
-export * from './types';
-export * from './parser';
-export { ContextBundleValidator } from './validator';
-export { ORMDParser as default } from './parser';
-//# sourceMappingURL=index.d.ts.map
--- packages/ormd-parser/dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * TypeScript types for ORMD (Organizational Relational Markdown) format
- * Based on the specification in docs/04_data_artifact_specification.ormd
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
